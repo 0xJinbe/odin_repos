f96b570eb3ae1112b8f08dc9ab3fe4567e2b4dbf
claudio-germano <wantuir.germano@gmail.com>
2022-07-14 11:50:35 -0300

    DOM tests

---
+++ odin_DOM/main.js
@@ -0,0 +1,17 @@
+const container = document.querySelector("#container")
+
+
+//1
+
+const p = document.createElement("p")
+p.style.color = "red"
+p.textContent = "This is a wonderfull red text creat by the DOM (Not the Corleone one lol)"
+container.append(p)
+
+//2
+
+const h3 = document.createElement("h3")
+p.style.color = "blue"
+p.textContent = "Im blue motherfucker"
+container.append(h1)
+
